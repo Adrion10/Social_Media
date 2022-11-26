48fc890c235402817fd4db74611603396be2e351
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-26 17:38:52 +0100

    configuration

--- server/index.js
+++ server/index.js
@@ -9,3 +9,18 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { fileURLToPath } from "url";
+
+/*configuration */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "corss-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
