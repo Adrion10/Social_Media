c2870006daeb6c30c2269358632a7e8818536b36
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-27 22:10:35 +0100

    user model

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+  picturePath: {
+    type: String,
+    default: "",
+  },
+});
