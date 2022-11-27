430a73adbbffc7c77d0ed2592d4d8fbeba66db7b
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-27 22:21:50 +0100

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,43 @@
 import mongoose from "mongoose";
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 3,
-    max: 20,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 20,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
   },
-  lastName: {
-    type: String,
-    required: true,
-    min: 3,
-    max: 20,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    max: 20,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 5,
-  },
-  picturePath: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
+const User = mongoose.model("User", UserSchema);
+export default User;
