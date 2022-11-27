36acd79bef3298533106fc443855a7c33c94987b
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-27 19:33:38 +0100

    const upload = multer({ storage });

--- server/index.js
+++ server/index.js
@@ -30,4 +30,8 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
   },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+const upload = multer({ storage });
