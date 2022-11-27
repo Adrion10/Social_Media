5a46e36110a2e05e4f291fb8e682b4da72040b73
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-27 22:44:40 +0100

    auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,36 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../modeles/User.js";
+import User from "../models/User.js";
+
+/*Register User */
+export const register = async (req, res) => {
+  try {
+  } catch (err) {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+  }
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+  const newUser = new User({
+    firstName,
+    lastName,
+    email,
+    password: passwordHash,
+    picturePath,
+    friends,
+    location,
+    occupation,
+    viewedProfile: Math.floor(Math.random() * 10000),
+    impressions: Math.floor(Math.random() * 10000),
+  });
+  const saveUser = await newUser.save();
+  res.status(201).json(saveUser);
+};
