cd30e628629c4ba307ae93e63cdaf05865835cc2
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-27 20:52:34 +0100

    env

--- server/index.js
+++ server/index.js
@@ -13,17 +13,15 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "corss-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
 /* File Sorage */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
