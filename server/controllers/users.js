dbf57b183748113f53f3ebe01798ec12812c9236
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-29 21:38:34 +0100

    /*Update */ export const addRemoveFriend

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
-/**Read */
 
+/**Read */
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
@@ -17,7 +17,36 @@ export const getUserFriend = async (req, res) => {
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
+/**Update */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findeById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
     const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
--- server/routes/users.js
+++ server/routes/users.js
@@ -16,6 +16,6 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 
 /**Update */
 
-router.patch("/:idfriendId", verifyToken, addRemoveFriend);
+router.patch("/:id/friendId", verifyToken, addRemoveFriend);
 
 export default router;
