b137fca78e68b58afb1844817497e576f50fd973
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-29 21:19:55 +0100

    export const getUserFriend = async (req, res) =>

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -10,3 +10,21 @@ export const getUser = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+export const getUserFriend = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
