1374df653b1aebf260961c38b896c16699c98ee9
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-30 21:02:40 +0100

    model Post

---
+++ server/models/Post.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const postShema = mongoose.Shema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    pictureePaath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+const Post = mongoose.model("Post", postShema);
+
+export default Post;
