4f4b55c496d697790560fd117c35c49880206d58
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-30 19:18:52 +0100

    post

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /*configuration */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +40,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 /*Rotes with files */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 /*Routs */
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
