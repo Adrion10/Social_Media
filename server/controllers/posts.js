4af9ffc75f102ac15a9869349d0b603ad3bb13b9
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-11-30 22:50:30 +0100

    export const createPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,23 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/* Create */
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      userPicturePath: user.picturePath,
+      description,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
