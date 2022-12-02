c57867dbd39c0942134b0679675d3c44c73ac773
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-02 23:32:44 +0100

    app router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/HomePage";
+import LoginPage from "scenes/LoginPage";
+import ProfilePage from "scenes/ProfilePage";
+
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/profile:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
