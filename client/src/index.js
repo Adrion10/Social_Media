6d1aa1b72c6772b60db76feeb5ec43a3faa1384d
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-03 19:13:50 +0100

    index.js Provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  presistStore,
+  presistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PRUGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = presistReducer(persistConfig, authReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
