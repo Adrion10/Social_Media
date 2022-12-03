58dc60b29722305245c96f22d34b33c7857cd3d1
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-03 20:00:36 +0100

    index fixed

--- client/src/index.js
+++ client/src/index.js
@@ -7,28 +7,25 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
-  presistReducer,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
-  PRUGE,
+  PERSIST,
+  PURGE,
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  version: 1,
-};
-const persistedReducer = presistReducer(persistConfig, authReducer);
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PRUGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
