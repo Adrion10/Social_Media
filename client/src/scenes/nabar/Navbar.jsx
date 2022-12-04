2fcfaf857a4436a8202777be24c9a2518cc159f6
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-04 22:27:21 +0100

    navbar import

--- client/src/scenes/nabar/Navbar.jsx
+++ client/src/scenes/nabar/Navbar.jsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FromControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+
 const Navbar = () => {
   return <div>Navbar</div>;
 };
