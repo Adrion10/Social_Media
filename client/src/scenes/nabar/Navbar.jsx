c0f9c77f184185474cd3929d82de89ff4823bd77
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-04 23:03:42 +0100

    Typography

--- client/src/scenes/nabar/Navbar.jsx
+++ client/src/scenes/nabar/Navbar.jsx
@@ -33,18 +33,23 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("min-width:1000px");
   const theme = useTheme();
-  const neutralLight = theme.palette.neutral.light
-  const dark = theme.palette.neutral.dark
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`
+  const fullName = `${user.firstName} ${user.lastName}`;
 
-  const handleLogout = () => {
-  return <FlexBetween padding= "1rem 6% backgroundColor = {alt">
-
-  </FlexBetween>
+  return (
+    <FlexBetween padding="1rem 6% backgroundColor = {alt">
+      <FlexBetween gap="1.75rem">
+        <Typography fontWeight="bold" fontSize="clamp(1rem,2rem,2.25rem">
+          Sociopedia
+        </Typography>
+      </FlexBetween>
+    </FlexBetween>
+  );
 };
 
 export default Navbar;
