c19431e4b51add13a26e9fce9ed8c8521213722b
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-06 19:44:07 +0100

    <Typography fontWeight="bold"

--- client/src/scenes/loginPage/LoginPage.jsx
+++ client/src/scenes/loginPage/LoginPage.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  return (
+    <Box>
+      <Typography fontWeight="bold" fontSize="clamp(32px)" color="primary">
+        Sociopedia
+      </Typography>
+    </Box>
+  );
 };
 
 export default LoginPage;
