ed71776ee457b545f4efaf6737a0ad5def6ac2c3
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-11 22:45:01 +0100

    const registerSchema

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -16,6 +16,16 @@ import { setLogin } from "state";
 import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
+
 const Form = () => {
   const [pageType, setPageType] = useState("login");
   const { palette } = useTheme();
