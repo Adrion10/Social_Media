94b14db4673fdfe7e90fb2c0d87981d23628b8bb
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-13 22:42:57 +0100

    <form onSubmit={handleSubmit}>

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -113,7 +113,15 @@ const Form = () => {
         setFieldValue,
         resetForm,
       }) => (
-
+        <form onSubmit={handleSubmit}>
+        <Box
+          display="grid"
+          gap="30px"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+          }}
+        >
   )
 };
 
