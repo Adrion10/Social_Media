74bc7a8db322c590f9f5d193170c6dc86e65f468
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-14 22:04:41 +0100

    const getUser = async

--- client/src/scenes/profilePage/ProfilePage.jsx
+++ client/src/scenes/profilePage/ProfilePage.jsx
@@ -13,6 +13,15 @@ const ProfilePage = () => {
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
   return <div>ProfilePage</div>;
 };
 
