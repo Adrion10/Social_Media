db257ce0382b956316cd8d9bcb5c3b2fee1ddb33
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-14 21:57:13 +0100

    <TextField               label="Password"

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -213,6 +213,18 @@ const Form = () => {
               helperText={touched.email && errors.email}
               sx={{ gridColumn: "span 4" }}
             />
+            <TextField
+              label="Password"
+              type="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 4" }}
+            />
+          </Box>
   )
 };
 
