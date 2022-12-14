6fc3e57df12a407bad6f1b4f31984676fff8c7ff
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-14 22:03:57 +0100

    useState

--- client/src/scenes/profilePage/ProfilePage.jsx
+++ client/src/scenes/profilePage/ProfilePage.jsx
@@ -9,6 +9,10 @@ import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return <div>ProfilePage</div>;
 };
 
