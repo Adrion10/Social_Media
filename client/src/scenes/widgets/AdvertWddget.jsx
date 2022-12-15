9f041c3026cc9f015860ee5e007901c4d6138e4d
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-15 22:20:44 +0100

    AdvertWidget

---
+++ client/src/scenes/widgets/AdvertWddget.jsx
@@ -0,0 +1,9 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWddget = () => {
+  return <div>AdvertWddget</div>;
+};
+
+export default AdvertWddget;
