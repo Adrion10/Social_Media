335bfba51d9d8bbf5be889eba37f789497eaef47
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-15 22:18:28 +0100

    box

--- client/src/scenes/profilePage/ProfilePage.jsx
+++ client/src/scenes/profilePage/ProfilePage.jsx
@@ -1,49 +1,36 @@
-import { Box, useMediaQuery } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import Navbar from "scenes/navbar";
-import FriendListWidget from "scenes/widgets/FriendListWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget";
-import PostsWidget from "scenes/widgets/PostsWidget";
-import UserWidget from "scenes/widgets/UserWidget";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
-const ProfilePage = () => {
-  const [user, setUser] = useState(null);
-  const { userId } = useParams();
-  const token = useSelector((state) => state.token);
-  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-
-  const getUser = async () => {
-    const response = await fetch(`http://localhost:3001/users/${userId}`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    const data = await response.json();
-    setUser(data);
-  };
-
-  useEffect(() => {
-    getUser();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (!user) return null;
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-  <Box>
-    <Navbar />
-    <Box
-      width="100%"
-      padding="2rem 6%"
-      display={isNonMobileScreens ? "flex" : "block"}
-      gap="2rem"
-      justifyContent="center"
-    >
-      <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-        <UserWidget userId={userId} picturePath={user.picturePath} />
-        <Box m="2rem 0" />
-        <FriendListWidget userId={userId} />
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
       </Box>
-      )
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
-export default ProfilePage;
+export default LoginPage;
