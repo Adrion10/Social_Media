b062013b39b8045113ce7c469e2862c6a9f58c52
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-15 22:22:33 +0100

    <FlexBetween>

--- client/src/scenes/widgets/AdvertWddget.jsx
+++ client/src/scenes/widgets/AdvertWddget.jsx
@@ -8,22 +8,30 @@ const AdvertWddget = () => {
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
   return (
-
     <WidgetWrapper>
-    <FlexBetween>
-      <Typography color={dark} variant="h5" fontWeight="500">
-        Sponsored
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>MikaCosmetics</Typography>
+        <Typography color={medium}>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light.
       </Typography>
-      <Typography color={medium}>Create Ad</Typography>
-    </FlexBetween>
-    <img
-      width="100%"
-      height="auto"
-      alt="advert"
-      src="http://localhost:3001/assets/info4.jpeg"
-      style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
-    />
-  )
+    </WidgetWrapper>
+  );
 };
 
 export default AdvertWddget;
