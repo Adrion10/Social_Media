4d935f293787238aeb1dbb4ffc466a9153882420
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-18 22:19:06 +0100

    const dispatch = useDispatch();

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -5,7 +5,12 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
 
-const FriendListWidget = () => {
+const FriendListWidget = ({ userId }) => {
+  const dispatch = useDispatch();
+  const { palette } = useTheme();
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
   return <div>FriendListWidget</div>;
 };
 
