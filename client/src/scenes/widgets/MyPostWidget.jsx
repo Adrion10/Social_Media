a51de0574e191ff53c6d2f7131836c88ac2f5cec
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-20 21:44:57 +0100

    const MyPostWidget

---
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,15 @@
+import {
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+
+const MyPostWidget = () => {
+  return <div>MyPostWidget</div>;
+};
+
+export default MyPostWidget;
