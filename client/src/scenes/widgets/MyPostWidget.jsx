516dbd0a70da2e61ba76542c87adf7df88d5932c
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-20 21:48:01 +0100

    const response

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -36,7 +36,7 @@ const MyPostWidget = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const mediumMain = palette.neutral.mediumMain;
   const medium = palette.neutral.medium;
-  
+
   const handlePost = async () => {
     const formData = new FormData();
     formData.append("userId", _id);
@@ -45,7 +45,16 @@ const MyPostWidget = () => {
       formData.append("picture", image);
       formData.append("picturePath", image.name);
     }
-
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
 
   return <div>MyPostWidget</div>;
 };
