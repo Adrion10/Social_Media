3f7067dc27b07a751695fd9dd5d0a4b83ec38ba6
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-21 22:22:36 +0100

    <Typography color={main} sx={{ mt: "1rem" }}>

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -54,6 +54,18 @@ const PostWidget = ({
         subtitle={location}
         userPicturePath={userPicturePath}
       />
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
+      {picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${picturePath}`}
+        />
+      )}
     </WidgetWrapper>
   );
 };
