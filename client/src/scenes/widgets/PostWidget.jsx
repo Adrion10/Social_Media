3ce6096f57a734edbad2c857bf620338fc561abb
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-21 22:10:25 +0100

    import { setPost } from "state";

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -4,6 +4,13 @@ import {
   FavoriteOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
 const PostWidget = () => {
   return <div>PostWidget</div>;
