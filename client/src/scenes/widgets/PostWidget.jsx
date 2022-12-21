ad675477bf5818a0ca1ae64fbdb15c90f4cacbf1
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-21 22:14:32 +0100

    method: "PATCH",

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -33,6 +33,19 @@ const PostWidget = ({
 
   const main = palette.neutral.main;
   const primary = palette.primary.main;
+  const patchLike = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
+
   return <div>PostWidget</div>;
 };
 
